68fb6d3bd869e68cdc198cd5e8c182121aa5355b
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-12-27 17:12:03 +0500

    commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nafbar from './Components/Nafbar'
+import Button from './Components/Button'
+
+const App = () => {
+  return (
+    <div>
+      <h1>hellow</h1>
+      <Nafbar/>
+      <Button />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Nafbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nafbar = () => {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet ratione sint enim veniam, minus molestias necessitatibus, quod vero unde cum minima! Voluptatum autem harum eum natus nemo repellendus modi inventore?</p>      
+    </div>
+  )
+}
+
+export default Nafbar
